c9d2ecb41cd9fde1ab9064712f196e4eb4375db3
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-03-09 21:43:34 +0100

    test

---
+++ src/main/java/be/ahmetergun/petclinic/PetclinicApplication.java
@@ -0,0 +1,14 @@
+package be.ahmetergun.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
