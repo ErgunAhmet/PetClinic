6016397ce7cf54231dab02132b02a2236f5a973b
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-03-20 19:12:47 +0100

    creating a map based services
    closes #6.

---
+++ PetClinic-data/src/main/java/be/ahmetergun/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package be.ahmetergun.petclinic.services.map;
+
+import be.ahmetergun.petclinic.model.Vet;
+import be.ahmetergun.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
