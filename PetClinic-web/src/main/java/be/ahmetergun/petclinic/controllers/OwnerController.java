edb3013b3058b4f234bb9a2fede6fe88ddd5761d
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-03-20 20:57:44 +0100

    adding an index page and controller to Owner
    closes #16.

---
+++ PetClinic-web/src/main/java/be/ahmetergun/petclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package be.ahmetergun.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
