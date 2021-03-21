d717806447117e96911365aa67b88b64c065554f
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-03-21 22:02:28 +0100

    list all vets from bootstrap Closes#15

--- PetClinic-web/src/main/java/be/ahmetergun/petclinic/controllers/VetController.java
+++ PetClinic-web/src/main/java/be/ahmetergun/petclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package be.ahmetergun.petclinic.controllers;
 
 
+import be.ahmetergun.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
