85c853fc434bcf0f0e9788f6bbc13ccfc77fc7d6
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-03-22 12:51:58 +0100

    refactoring map impl to manage settings id property. Closes#23

--- PetClinic-data/src/main/java/be/ahmetergun/petclinic/services/map/AbstractMapService.java
+++ PetClinic-data/src/main/java/be/ahmetergun/petclinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package be.ahmetergun.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import be.ahmetergun.petclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -14,8 +13,16 @@ public abstract class AbstractMapService<T, ID> {
     T findById(ID id){
         return map.get(id);
     }
-    T save(ID id, T object){
-        map.put(id, object);
+    T save(T object){
+        if(object != null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }else {
+            throw new RuntimeException("object cannot be null");
+        }
+
         return object;
     }
     void deleteById(ID id){
@@ -23,5 +30,15 @@ public abstract class AbstractMapService<T, ID> {
     }
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+    private Long getNextId(){
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e){
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 }
