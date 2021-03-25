9643fb2b679cc9af13d1c5ab599062631bf1b8b5
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-03-25 23:34:58 +0100

    convert visit to jpa entities. Closes #38

--- PetClinic-data/src/main/java/be/ahmetergun/petclinic/model/Visit.java
+++ PetClinic-data/src/main/java/be/ahmetergun/petclinic/model/Visit.java
@@ -1,10 +1,17 @@
 package be.ahmetergun.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-
-public class Visit {
+@Entity
+@Table(name = "visits")
+public class Visit extends BaseEntity{
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
