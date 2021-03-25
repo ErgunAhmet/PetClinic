9b72bc32305c523f7903000940c6a3e40721b115
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-03-26 00:53:35 +0100

    create visit map service Closes #46

---
+++ PetClinic-data/src/main/java/be/ahmetergun/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package be.ahmetergun.petclinic.services;
+
+import be.ahmetergun.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
---
+++ PetClinic-data/src/main/java/be/ahmetergun/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,40 @@
+package be.ahmetergun.petclinic.services.map;
+
+import be.ahmetergun.petclinic.model.Visit;
+import be.ahmetergun.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.getPet() == null
+                || object.getPet().getOwner() == null
+                || object.getPet().getId() == null
+                || object.getPet().getOwner().getId() == null){
+            throw new RuntimeException("invalid Visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
